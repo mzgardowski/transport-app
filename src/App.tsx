05855ef906f016a8488b7a90f9fa1e2a86da7dff
Mateusz Zgardowski <m.zgardowski@global4net.com>
2023-01-02 15:39:06 +0100

    eslint, tests, ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Logo from "./logo.svg";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Logo as unknown as string} className="App-logo" alt="logo" />
+        {/* <Logo /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
